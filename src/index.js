4f1da68430e1a4c67a89318eaf2677eae691f5b6
Renan Humberto Vitorino da Silva <renanhumberto61@gmail.com>
2025-03-28 16:36:27 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,6 +8,6 @@ const port = 3000;
 
 
 app.use(express.json()); //adiciona suporte para JSON
-app.use("/cadastro", cadastroRoute);
+app.use("/cadastro", cadastroRoute); //testando git pull
 
 app.listen(3000, () => console.log(`✅Servidor rodando na porta ${port}`));
